29c8c93f165a785cd53d496d18934fce24657594
kimyuchan <rladbcks103@naver.com>
2025-11-30 21:18:49 +0900

    Disable KakaoCommunityApplicationTests

--- src/test/java/org/example/kakaocommunity/KakaoCommunityApplicationTests.java
+++ src/test/java/org/example/kakaocommunity/KakaoCommunityApplicationTests.java
@@ -3,6 +3,7 @@ package org.example.kakaocommunity;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class KakaoCommunityApplicationTests {
 
